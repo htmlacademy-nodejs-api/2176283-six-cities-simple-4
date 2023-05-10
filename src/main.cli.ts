d23d77679d307520881da93394f60d75626c6fb3
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-11 00:11:28 +0300

    1.5 - Добавляем файл с тестовыми данными - Добавляем типы для импортируемых сущностей - Добавляем класс для чтения tsv-файлов

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,5 +1,6 @@
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/helper.command.js';
+import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
 
 /**
@@ -8,7 +9,7 @@ import VersionCommand from './core/cli-command/version.command.js';
 const myManager = new CLIApplication();
 
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand
+  new HelpCommand, new VersionCommand, new ImportCommand
 ]);
 
 myManager.processCommand(process.argv);
