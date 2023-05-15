e3439a4c990f3cfebf8149160b34ec9723babb03
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-15 22:10:58 +0300

    2.8 - Добавим команду 'GenerateCommand' для формирования объявлений

---
+++ src/core/cli-command/generate-command.ts
@@ -0,0 +1,15 @@
+import { CliCommandInterface } from './cli-command.interface.js';
+import { MockData } from '../../types/mock-data.type.js';
+
+/**
+ * Команда для формирования объявлений
+ */
+export default class GenerateCommand implements CliCommandInterface {
+  public readonly name = '--generate';
+  private initialData!: MockData;
+
+  public async execute(...parameters: string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+  }
+}
