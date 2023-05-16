506f8cfaf6e1fd7fcce0dff52a3781ee7a916f4b
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-16 22:30:20 +0300

    2.13 - Рефакторинг. Функция createOffer. WIP Приложение работает, но есть проблема: при попытке импортировать большой файл (> 2GB) возникнет ошибка — `ERR_FS_FILE_TOO_LARGE`.
    Можно предположить, что проблема проявляется из-за использования
    синхронной функции `readFileSync`. Но это не так. Переход на асинхронный
    вариант в виде `readFile` ничего не решит. Ошибка сохранится.
    
    Node.js ограничивает размер буфера, в который считывается файл. Размер
    буфера зависит от версии Node.js (x32/x64). В x64 он равен
    приблизительно 4GB. Однако в функциях для работы с файлами есть
    дополнительное ограничение — 2GB.
    
    Такие ограничения введены намеренно. Считываемый файл загружается
    целиком в оперативную память и памяти может не хватить. Да и вряд ли
    вам потребуется огроменный файл в оперативной памяти.
    
    Прочитать и поработать с большими файлами помогут потоки (Streams).
    
    Подходить к работе с потоками начнём с рефакторинга. Перенесли код для создания объявления из строки в отдельную функцию `createOffer`

--- src/core/file-reader/tsv-file-reader.ts
+++ src/core/file-reader/tsv-file-reader.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from 'node:fs';
 import { FileReaderInterface } from './file-reader.interface.js';
-import { Offer } from '../../types/offer.type.js';
 
 /**
  * Класс для чтения tsv-файлов
@@ -12,35 +11,5 @@ export default class TSVFileReader implements FileReaderInterface {
 
   public read(): void {
     this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
-  }
-
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      return [];
-    }
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([title, description, date, city, previewImage, images, isPremium, rating, type, bedrooms, maxAdults, price, goods, nick, email, avatar, password, isPro, latitude, longitude]) => ({
-        title,
-        description,
-        date: new Date(date),
-        city,
-        previewImage,
-        images: images.split(';')
-          .map((name) => ({name})),
-        isPremium: Boolean(isPremium),
-        rating: Number.parseFloat(rating),
-        type,
-        bedrooms: Number.parseInt(bedrooms, 10),
-        maxAdults: Number.parseInt(maxAdults, 10),
-        price: Number.parseInt(price, 10),
-        goods: goods.split(';')
-          .map((name) => ({name})),
-        host: {nick, email, avatar, password, isPro: Boolean(isPro)},
-        location: {latitude: Number.parseFloat(latitude), longitude: Number.parseFloat(longitude)},
-      }));
   }
 }
