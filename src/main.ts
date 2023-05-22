1993de6eba98e562a3c8d8c4405954724d65b883
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-22 23:36:37 +0300

    3.19. Добавим контейнер с зависимостями. WIP В файле `main.ts` (точка входа) создаём экземпляры всех необходимых классов и зависимостей для них. Попробуем сделать то же самое с помощью возможностей `inversify`.
    Первым делом создадим контейнер. В контексте библиотеки `inversify`,
    контейнер — это место, где происходит настройка и привязка зависимостей.
    
    При создании контейнера мы объясняем `inversify` какую реализацию
    следует использовать для определённого интерфейса.
    
    Например, интерфейсу `LoggerInterface` соответствует реализация в
    виде класса `PinoService`. А интерфейсу `ConfigInterface` реализация
    `ConfigService` и так далее.
    
    За счёт построения контейнера, в любом другом классе мы сможем получить
    нужную реализацию. При этом `inversify` возьмёт на себя создание
    необходимых экземпляров.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import PinoService from './core/logger/pino.service.js';
 import RestApplication from './app/rest.js';
 import ConfigService from './core/config/config.service.js';
+import { Container } from 'inversify';
+import { AppComponent } from './types/app-component.enum.js';
+import { LoggerInterface } from './core/logger/logger.interface.js';
+import { RestSchema } from './core/config/rest.schema.js';
+import { ConfigInterface } from './core/config/config.interface.js';
 
 /**
  * Функция для создания экземпляра приложения
  */
 async function bootstrap() {
-  const logger = new PinoService();
-  const config = new ConfigService(logger);
 
-  const application = new RestApplication(logger, config);
+  const container = new Container();
+
+  container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication);
+  container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService);
+  container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService);
+
+  const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
 }
 
