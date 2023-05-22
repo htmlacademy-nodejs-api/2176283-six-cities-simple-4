01acf694e73ddd09d41677127657a52d93851392
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-22 21:57:33 +0300

    3.18. Добавим перечисление AppComponent. Подготовим новый модуль с перечислением компонентов приложения, которые будут участвовать при внедрении зависимостей.
    Новый модуль создаём в директории `types`, хотя фактически это не
    является описание типа в контексте TypeScript. Мы описываем простой
    константный объект. Ключи объекта — название компонентов, а если точней
    интерфейсов. Значения ключей — символы. Можно использовать строки, но
    символы уникальны «из коробки».
    
    Объект нам потребуется при подготовке контейнера.

---
+++ src/types/app-component.enum.ts
@@ -0,0 +1,5 @@
+export const AppComponent = {
+  RestApplication: Symbol.for('RestApplication'),
+  LoggerInterface: Symbol.for('LoggerInterfase'),
+  ConfigInterface: Symbol.for('ConfigInterface'),
+} as const;
