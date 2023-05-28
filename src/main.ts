5329608fdce0a04ef8a13ce7b64f256f6a8d688b
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-28 17:09:10 +0300

    4.8. Добавим привязку для DatabaseClientInterface. Добавим в контейнер привязку для `DatabaseClientInterface`. Свяжем с реализацией `MongoClientService`. Достаточно одного экземпляра.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './types/app-component.enum.js';
 import { LoggerInterface } from './core/logger/logger.interface.js';
 import { RestSchema } from './core/config/rest.schema.js';
 import { ConfigInterface } from './core/config/config.interface.js';
+import { DatabaseClientInterface } from './core/database-client/database-client.interface.js';
+import MongoClientService from './core/database-client/mongo-client.service.js';
 
 /**
  * Функция для создания экземпляра приложения
@@ -23,6 +25,9 @@ async function bootstrap() {
 
   container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface)
     .to(ConfigService).inSingletonScope();
+
+  container.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface)
+    .to(MongoClientService).inSingletonScope();
 
   const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
