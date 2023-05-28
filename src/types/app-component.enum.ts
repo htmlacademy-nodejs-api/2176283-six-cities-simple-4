1397a9b86fe417b7599db87ddf8f3fd2c6e8a822
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-28 16:58:11 +0300

    4.7. Добавим новый компонент в AppComponent. В перечисление `AppComponent` добавим значение — `DatabaseInterface`. Это необходимо для интерфейса `DatabaseClientInterface` с определённым классом в IoC-контейнере.

--- src/types/app-component.enum.ts
+++ src/types/app-component.enum.ts
@@ -1,5 +1,10 @@
+/**
+ * Объект с интерфейсами приложения
+ * Интерфейсы участвуют при внедрении зависимостей
+ */
 export const AppComponent = {
   RestApplication: Symbol.for('RestApplication'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseClientInterface: Symbol.for('DatabaseClientInterface'),
 } as const;
