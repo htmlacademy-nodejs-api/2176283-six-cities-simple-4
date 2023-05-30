482243590354a8cd04cf3e1da3f495e116782f86
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-05-31 00:35:47 +0300

    4.17. Добавим определение модели и схемы. WIP
    За счёт декоратора `@prop` мы описываем схему как бы
    поверх класса. У класса могут быть и другие свойства, которые не должны
    сохраняться в базу данных, а использоваться только на уровне модели, для
    каких-то служебных целей.
    
    Функция — `getModelForClass` создаст объект модели.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,10 +1,25 @@
 import { User } from '../../types/user.type';
+import typegoose, { getModelForClass } from '@typegoose/typegoose';
+
+const {prop} = typegoose;
 
 // Сущность User
 export class UserEntity implements User {
+
+  @prop({required: true})
   public nick = '';
+
+  @prop({unique: true, required: true})
   public email = '';
+
+  @prop({required: false, default: ''})
   public avatar = '';
+
+  @prop({required: true})
   public password = '';
+
+ @prop({required: false})
   public isPro = false;
 }
+
+export const UserModel = getModelForClass(UserEntity);
