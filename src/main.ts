740bfd8e806057f7424f72e1b91017bef11c108e
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-06-01 21:17:25 +0300

    4.24. Разобьём общий контейнер. WIP Метод `merge()` предназначен для разделения одного большого контейнера зависимостей на несколько.
    Сейчас в контейнере всего четыре зависимости: `Application`, `LoggerService`, `ConfigService` и `DatabaseService`. Однако дальнейшая работа предполагает управление большим количеством различных сервисов. Точка входа `main.ts` может превратиться в очень длинный листинг подключенных компонентов.
    
    Разбьём общий контейнер зависимостей на нескольно контейнеров,
    описывающих свою предметную область. Метод `merge()` принимает два и более
    контейнеров зависимостей и собирает их в один общий.
    
    Обернем вызовы функций в точке входа (`main.ts`) в функцию
    `bootstrap()`.

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,17 @@
 import 'reflect-metadata';
-import PinoService from './core/logger/pino.service.js';
 import RestApplication from './app/rest.js';
-import ConfigService from './core/config/config.service.js';
 import { Container } from 'inversify';
 import { AppComponent } from './types/app-component.enum.js';
-import { LoggerInterface } from './core/logger/logger.interface.js';
-import { RestSchema } from './core/config/rest.schema.js';
-import { ConfigInterface } from './core/config/config.interface.js';
-import { DatabaseClientInterface } from './core/database-client/database-client.interface.js';
-import MongoClientService from './core/database-client/mongo-client.service.js';
+import { createRestApplicationContainer } from './app/rest.container.js';
 
 /**
- * Функция для создания экземпляра приложения
+ * Функция объединения контейнеров приложения
  */
 async function bootstrap() {
 
-  const container = new Container();
+  const minContainer = Container.merge(createRestApplicationContainer());
 
-  container.bind<RestApplication>(AppComponent.RestApplication)
-    .to(RestApplication).inSingletonScope();
-
-  container.bind<LoggerInterface>(AppComponent.LoggerInterface)
-    .to(PinoService).inSingletonScope();
-
-  container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface)
-    .to(ConfigService).inSingletonScope();
-
-  container.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface)
-    .to(MongoClientService).inSingletonScope();
-
-  const application = container.get<RestApplication>(AppComponent.RestApplication);
+  const application = minContainer.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
 }
 
