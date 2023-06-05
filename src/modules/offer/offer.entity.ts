c140353e119a57aa57637aad55536f7a4af9ef55
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-06-05 22:50:40 +0300

    Правки к 4... Проверки полей сущьности 'Offer'

--- src/modules/offer/offer.entity.ts
+++ src/modules/offer/offer.entity.ts
@@ -1,6 +1,8 @@
 import typegoose, { Ref, defaultClasses, getModelForClass} from '@typegoose/typegoose';
 import { Location } from '../../types/location.type.js';
 import { UserEntity } from '../user/user.entity.js';
+import { OfferCity } from '../../types/offer-city.enum.js';
+import { OfferType } from '../../types/offer-type.enum.js';
 
 const {prop, modelOptions} = typegoose;
 
@@ -17,17 +19,29 @@ export interface OfferEntity extends defaultClasses.Base {}
 // Сущность Offer
 export class OfferEntity extends defaultClasses.TimeStamps {
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    minlength: 10,
+    maxlength: 100
+  })
   public title!: string;
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    minlength: 20,
+    maxlength: 1024
+  })
   public description!: string;
 
   @prop({required: true})
   public date!: Date;
 
-  @prop({required: true})
-  public city!: string;
+  @prop({
+    required: true,
+    type: () => String,
+    enum: OfferCity
+  })
+  public city!: OfferCity;
 
   @prop({required: true})
   public previewImage!: string;
@@ -41,19 +55,39 @@ export class OfferEntity extends defaultClasses.TimeStamps {
   })
   public isPremium!: boolean;
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    min: 1,
+    max: 5
+  })
   public rating!: number;
 
-  @prop({required: true})
-  public type!: string;
+  @prop({
+    required: true,
+    type: () => String,
+    enum: OfferType
+  })
+  public type!: OfferType;
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    min: 1,
+    max: 8
+  })
   public bedrooms!: number;
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    min: 1,
+    max: 10
+  })
   public maxAdults!: number;
 
-  @prop({required: true})
+  @prop({
+    required: true,
+    min: 100,
+    max: 100000
+  })
   public price!: number;
 
   @prop({required: true})
