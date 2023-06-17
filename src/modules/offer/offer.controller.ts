1484cde6d957446e672900f804b529d63a97931b
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-06-18 01:04:28 +0300

    6.9. Добавим create в OfferController.
    Данные для создания офера возьмём из тела запроса. Для этого
    обратимся к полю `body` объекта `Request`. Мы воспользовались
    middleware `express.json()`, поэтому в `body` будет обычный объект.
    
     Для проверки обработчика создадим тестовый запрос.

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -7,6 +7,7 @@ import { HttpMethod } from '../../types/http-method.enum.js';
 import { OfferServiceInterface } from './offer-service.interface.js';
 import { fillDTO } from '../../core/helpers/index.js';
 import OfferRdo from './rdo/offer.rdo.js';
+import CreateOfferDto from './dto/create-offer.dto.js';
 
 @injectable()
 export default class OfferController extends Controller {
@@ -19,12 +20,20 @@ export default class OfferController extends Controller {
     this.logger.info('Register routes for OfferController...');
 
     this.addRoute({path: '/', method: HttpMethod.Get, handler: this.list});
+    this.addRoute({path: '/', method: HttpMethod.Post, handler: this.create});
   }
 
   public async list(_req: Request, res: Response): Promise<void> {
     const offers = await this.offerService.find();
     const offersToResponce = fillDTO(OfferRdo, offers);
     this.ok(res, offersToResponce);
+  }
+
+  public async create({body}: Request<Record<string, unknown>, Record<string, unknown>, CreateOfferDto>, res: Response): Promise<void> {
+
+    const result = await this.offerService.create(body);
+    //const offer = await this.offerService.findOfferById(result.id);
+    this.created(res, fillDTO(OfferRdo, result));
   }
 
 }
